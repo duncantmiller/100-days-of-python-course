b0636cc0f658253275ea9b6681da8e75ccd08ac9
Duncan Miller <dmiller@heatspring.com>
2023-08-18 10:17:31 -0700

    test files

---
+++ band_name_generator.py
@@ -0,0 +1,3 @@
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city_name + " " + pet_name)
