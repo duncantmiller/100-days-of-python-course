c4b45be9bfbefbd6ddeb03c671a859402a3f4e56
Duncan Miller <dmiller@heatspring.com>
2023-11-13 15:20:20 -0800

    adding instructions to love calculator

--- love_calculator.py
+++ love_calculator.py
@@ -1,3 +1,25 @@
+# Instructions
+# 💪 This is a difficult challenge! 💪
+# You are going to write a program that tests the compatibility between two people.
+
+# To work out the love score between two people:
+
+# Take both people's names and check for the number of times the letters in the word TRUE occurs.
+
+# Then check for the number of times the letters in the word LOVE occurs.
+
+# Then combine these numbers to make a 2 digit number.
+
+# For Love Scores less than 10 or greater than 90, the message should be:
+
+# "Your score is *x*, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+
+# "Your score is *y*, you are alright together."
+# Otherwise, the message will just be their score. e.g.:
+
+# "Your score is *z*."
+
 print("The Love Calculator is calculating your score")
 name1 = input("Enter the first person's full name: ")
 name2 = input("Enter the second person's full name: ")
