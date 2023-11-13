0073a93a9bee35d5e262ec07f53eb863b4af9998
Duncan Miller <dmiller@heatspring.com>
2023-11-13 13:24:12 -0800

    add pricing conditionals for ticket window

--- ticket_window.py
+++ ticket_window.py
@@ -3,5 +3,13 @@ height = int(input("Enter your height in inches: "))
 
 if height >= 48:
   print("You can ride the rollercoaster")
+  age = int(input("Enter your age in years: "))
+  if age < 12:
+    price = 5
+  elif age <= 18:
+    price = 7
+  else:
+    price = 12
+  print(f"Please pay ${price}.00 to ride this ride")
 else:
   print("Sorry you are not tall enough")
