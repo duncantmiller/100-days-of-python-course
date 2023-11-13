2cba2e0c10565509ac2f51d9250d8b05a0489ee1
Duncan Miller <dmiller@heatspring.com>
2023-11-13 13:12:39 -0800

    use ternary operator

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,9 +1,6 @@
 print("Welcome to the odd or even bot")
 number = int(input("Enter a number: "))
 
-if number % 2 == 0:
-  odd_or_even = "even"
-else:
-  odd_or_even = "odd"
+odd_or_even = "even" if number % 2 == 0 else "odd"
 
 print(f"{number} is an {odd_or_even} number!")
