3e52fbf019a13fe9ac0a1443ba91577a40814a4f
Duncan Miller <dmiller@heatspring.com>
2023-11-13 12:41:15 -0800

    bill split tip calculator

---
+++ bill_split_tip_calculator.py
@@ -0,0 +1,14 @@
+print("Enter the total bill abount")
+bill_amount = float(input())
+print("What percentage tip would you like to give? 15, 20 or 25?")
+tip_amount = int(input())
+print("How many people to split the bill?")
+people = int(input())
+
+total = bill_amount * (1 + (tip_amount / 100))
+rounded_total = round(total, 2)
+
+per_person = (rounded_total / people)
+per_person_rounded = round(per_person, 2)
+
+print(f"Each person should pay ${per_person_rounded}")
