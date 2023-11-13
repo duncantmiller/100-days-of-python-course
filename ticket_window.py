081c0fad3c3ad03b819032c93ca816ed091274a5
Duncan Miller <dmiller@heatspring.com>
2023-11-13 14:17:29 -0800

    add midlife crisis tickets and message about ticket prices

--- ticket_window.py
+++ ticket_window.py
@@ -6,10 +6,17 @@ if height >= 48:
   age = int(input("Enter your age in years: "))
   if age < 12:
     price = 5
+    age_category = "Child"
   elif age <= 18:
     price = 7
+    age_category = "Youth"
+  elif age >= 45 and age <= 55:
+    price = 0
+    age_category = "Midlife crisis"
   else:
     price = 12
+    age_category = "Adult"
+  print (f"{age_category} tickets are ${price}.00")
   want_photos = input("Do you want photos? Answer yes or no: ")
   if want_photos == "yes":
     price += 3
