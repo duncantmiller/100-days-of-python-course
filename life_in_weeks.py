e935f03f16cd32540123ab9abe3108553ea1762c
Duncan Miller <dmiller@heatspring.com>
2023-11-13 12:25:09 -0800

    add conditionals for possible entries over 90

--- life_in_weeks.py
+++ life_in_weeks.py
@@ -1,7 +1,14 @@
 print("enter your age")
-age = input()
+age = int(input())
 end_years = 90
 
-years_left = end_years - int(age)
-weeks_left = years_left * 52
-print(f"You only have {weeks_left} weeks left until you are {end_years} old!")
+if age < end_years:
+  years_left = end_years - age
+  weeks_left = years_left * 52
+  print(f"You only have {weeks_left} weeks left until you are {end_years} years old!")
+elif age == end_years:
+  print(f"Congratulations, you've reached {end_years} years old (the end is near)!")
+else:
+  years_over = age - end_years
+  weeks_over = years_over * 52
+  print(f"You are already {weeks_over} weeks over expiration ({end_years} years)!")
