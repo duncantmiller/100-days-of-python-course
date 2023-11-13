99d09f8bfa6925317bc8b227ca2ea3df8c4e0b90
Duncan Miller <dmiller@heatspring.com>
2023-11-13 13:35:23 -0800

    add response method to bmi calculator

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,3 +1,17 @@
+def response(bmi_float):
+  if bmi_float < 18.5:
+    return "you are underweight"
+  elif bmi_float < 25:
+    return "you are normal weight"
+  elif bmi_float < 25:
+    return "you are normal weight"
+  elif bmi_float < 30:
+    return "you are slightly overweight"
+  elif bmi_float < 35:
+    return "you are obese"
+  else:
+    return "you are clinically obese"
+
 print("Enter your height in meters")
 height = input()
 print("Enter your weight in meters")
@@ -6,4 +20,4 @@ weight = input()
 bmi_float = int(weight) / (float((height)) ** 2)
 bmi_rounded = round(bmi_float, 0)
 bmi = int(bmi_rounded)
-print(f"Your BMI is {bmi}")
+print(f"Your BMI is {bmi} and {response(bmi_float)}")
