179d05f046c8a4a4bec7243f6b5115e8f767deb0
Duncan Miller <dmiller@heatspring.com>
2023-11-13 13:04:11 -0800

    ticket window conditionals

---
+++ ticket_window.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("Enter your height in inches: "))
+
+if height >= 48:
+  print("You can ride the rollercoaster")
+else:
+  print("Sorry you are not tall enough")
