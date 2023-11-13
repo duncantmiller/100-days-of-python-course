84ec9b481169c48e900055bb2aaf347234db03b2
Duncan Miller <dmiller@heatspring.com>
2023-11-13 12:01:55 -0800

    two digit calculator

---
+++ two_digit_calculator.py
@@ -0,0 +1,6 @@
+print("type a two digit number")
+two_digit_number = input()
+digit_one = int(two_digit_number[0])
+digit_two = int(two_digit_number[1])
+
+print("the sum of these two digits is " + str(digit_one + digit_two))
