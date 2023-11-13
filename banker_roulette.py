ef1345b4cfcad6a169ce4b689133ae6aceb900cb
Duncan Miller <dmiller@heatspring.com>
2023-11-13 15:24:29 -0800

    banker roulette working

--- banker_roulette.py
+++ banker_roulette.py
@@ -5,5 +5,11 @@
 
 # Line 1 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
 
+import random
+
 names_string = "Angela, Ben, Jenny, Michael, Chloe"
 names = names_string.split(", ")
+number_of_names = len(names)
+buyer = names[random.randint(0, number_of_names - 1)]
+
+print(f"{buyer} is going to buy the meal today!")
