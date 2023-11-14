310473f4327a4597e68ab1451311679d4fe6a22c
Duncan Miller <dmiller@heatspring.com>
2023-11-14 15:34:26 -0800

    hangman stage 4

--- Section_7_hangman/hangman.py
+++ Section_7_hangman/hangman.py
@@ -1,8 +1,68 @@
 import random
-#Step 1
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+lives = 6
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#Set 'lives' to equal 6.
 
 print(f"testing {chosen_word}")
 
@@ -21,8 +81,21 @@ while display.count("_") > 0:
         for index, letter in enumerate(chosen_word):
             if guess == letter:
                 display[index] = letter
+    else:
+        lives -= 1
+        print(f"Sorry {guess} is not in the word, you loose a life. Lives remaining: {lives}")
+        print(stages[lives])
+        if lives == 0:
+            print("You loose.")
+            exit()
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1.
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
+    #Join all the elements in the list and turn it into a String.
     print(display)
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
 
 print("You win!")
 
