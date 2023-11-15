b54769c8827b67c64091375ce301e287c4e24eb2
Duncan Miller <dmiller@heatspring.com>
2023-11-15 14:21:04 -0800

    wrap calculation in function and call recursively

--- Section_10_function_outputs/calculator.py
+++ Section_10_function_outputs/calculator.py
@@ -17,23 +17,31 @@ calculator_functions = {
     "/": divide,
 }
 
-number1 = int(input("What is the first number: "))
+def calculator():
+    number1 = int(input("What is the first number: "))
+    continue_calculation = True
+    while continue_calculation:
+        number2 = int(input("What is the second number: "))
+        print("Operations available:")
+        for symbol in calculator_functions:
+            print(symbol)
+        symbol = input("What operation symbol would you like to perform?: ")
+        calculator_function = calculator_functions[symbol]
+        answer = calculator_function(number1, number2)
+        print(f"{number1} {symbol} {number2} = {answer}")
+        more_calculations = input(
+            f"Would you like to continue performing calculations on {answer}? Type 'yes' or 'no'\n"
+        )
+        if more_calculations == "yes":
+            number1 = answer
+        else:
+            continue_calculation = False
+            more_calculations = input(
+                f"Would you like to peform a new calculation? Type 'yes' or 'no'\n"
+            )
+            if more_calculations == "yes":
+                calculator()
+            else:
+                print("Goodbye.")
 
-continue_calculation = True
-while continue_calculation:
-    number2 = int(input("What is the second number: "))
-    print("Operations available:")
-    for symbol in calculator_functions:
-        print(symbol)
-    symbol = input("What operation symbol would you like to perform?: ")
-    calculator_function = calculator_functions[symbol]
-    answer = calculator_function(number1, number2)
-    print(f"{number1} {symbol} {number2} = {answer}")
-    more_calculations = input(
-        f"Would you like to continue performing calculations on {answer}? Type 'yes' or 'no'\n"
-    )
-    if more_calculations == "yes":
-        number1 = answer
-    else:
-        continue_calculation = False
-        print("Goodbye.")
+calculator()
