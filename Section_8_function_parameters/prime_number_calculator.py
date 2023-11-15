cbf55fa0ae2f64d306f64aa1afd757f3c169a4b5
Duncan Miller <dmiller@heatspring.com>
2023-11-15 10:33:35 -0800

    correcting calculator for large prime numbers that are multiples of other prime numbers

--- Section_8_function_parameters/prime_number_calculator.py
+++ Section_8_function_parameters/prime_number_calculator.py
@@ -8,7 +8,7 @@ def prime_checker(number):
 def is_prime(number):
     if number == 1:
         return False
-    for n in range(2, 10):
+    for n in range(2, int(number**0.5) + 1):
         if number != n:
             if number % n == 0:
                 return False
