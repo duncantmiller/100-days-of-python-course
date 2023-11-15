000d0534f463feaa02d30323fbd4f3f3ef88f806
Duncan Miller <dmiller@heatspring.com>
2023-11-15 14:15:25 -0800

    refactoring duplication

--- Section_10_function_outputs/calculator.py
+++ Section_10_function_outputs/calculator.py
@@ -18,35 +18,22 @@ calculator_functions = {
 }
 
 number1 = int(input("What is the first number: "))
-number2 = int(input("What is the second number: "))
-print("Operations available:")
-for symbol in calculator_functions:
-    print(symbol)
-symbol = input("What operation symbol would you like to perform?: ")
-calculator_function = calculator_functions[symbol]
-answer = calculator_function(number1, number2)
-print(f"{number1} {symbol} {number2} = {answer}")
-more_calculations = input(
-    f"Would you like to continue performing calculations on {answer}? Type 'yes' or 'no'\n"
-)
-if more_calculations == "yes":
-    continue_calculation = True
-else:
-    continue_calculation = False
-    print("Goodbye.")
+
+continue_calculation = True
 while continue_calculation:
     number2 = int(input("What is the second number: "))
+    print("Operations available:")
     for symbol in calculator_functions:
         print(symbol)
     symbol = input("What operation symbol would you like to perform?: ")
     calculator_function = calculator_functions[symbol]
-    new_answer = calculator_function(answer, number2)
-    print(f"{answer} {symbol} {number2} = {new_answer}")
+    answer = calculator_function(number1, number2)
+    print(f"{number1} {symbol} {number2} = {answer}")
     more_calculations = input(
         f"Would you like to continue performing calculations on {answer}? Type 'yes' or 'no'\n"
     )
     if more_calculations == "yes":
-        continue_calculation = True
+        number1 = answer
     else:
         continue_calculation = False
         print("Goodbye.")
