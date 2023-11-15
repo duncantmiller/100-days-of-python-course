a7175b5994f524027099bb92e9407dc8f04341ff
Duncan Miller <dmiller@heatspring.com>
2023-11-15 11:17:35 -0800

    adding decrypt method

--- Section_8_function_parameters/ceasar_cipher.py
+++ Section_8_function_parameters/ceasar_cipher.py
@@ -7,6 +7,15 @@ def encrypt(text, shift):
         encrypted_text += alphabet[new_position]
     return encrypted_text
 
+def decrypt(text, shift):
+    shift = _normalize_shift(shift)
+    decrypted_text = ''
+    for letter in text:
+        current_position = alphabet.index(letter)
+        new_position = _normalize_position(current_position - shift)
+        decrypted_text += alphabet[new_position]
+    return decrypted_text
+
 def _normalize_shift(shift):
     if shift > 26:
         wraps = int(shift / 26)
@@ -26,14 +35,17 @@ shift = int(input("Type the shift number:\n"))
 
 if direction == "encode":
     print(encrypt(text, shift))
+elif direction == "decode":
+    print(decrypt(text, shift))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+  #e.g.
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
