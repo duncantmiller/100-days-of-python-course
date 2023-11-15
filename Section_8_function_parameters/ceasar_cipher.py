36422a9a8bf1ee95c76310426f21bd0e322e6bc5
Duncan Miller <dmiller@heatspring.com>
2023-11-15 11:51:26 -0800

    adding logo

--- Section_8_function_parameters/ceasar_cipher.py
+++ Section_8_function_parameters/ceasar_cipher.py
@@ -1,3 +1,5 @@
+import art
+
 def shifted_text(text, shift, direction):
     shift = _normalize_shift(shift)
     new_text = ''
@@ -25,6 +27,7 @@ def _normalize_position(position):
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(art.logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -34,14 +37,14 @@ if direction == "encode" or direction == "decode":
 else:
     print("Invalide encryption option, must be 'encode' or 'decode'")
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+#TODO-1: Import and print the logo from art.py when the program starts.
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
-  #e.g.
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
+#TODO-2: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
 
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+#TODO-3: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+#Hint: Think about how you can use the modulus (%).
