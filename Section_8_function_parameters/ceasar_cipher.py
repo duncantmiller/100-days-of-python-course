aebf17237c98fbf7361fdfe981276046a98f4356
Duncan Miller <dmiller@heatspring.com>
2023-11-15 12:00:18 -0800

    allow numbers and symbols to be passed to cipher

--- Section_8_function_parameters/ceasar_cipher.py
+++ Section_8_function_parameters/ceasar_cipher.py
@@ -4,9 +4,12 @@ def shifted_text(text, shift, direction):
     shift = _normalize_shift(shift)
     new_text = ''
     for letter in text:
-        current_position = alphabet.index(letter)
-        new_position = _reposition(current_position, shift, direction)
-        new_text += alphabet[new_position]
+        if alphabet.count(letter) > 0:
+            current_position = alphabet.index(letter)
+            new_position = _reposition(current_position, shift, direction)
+            new_text += alphabet[new_position]
+        else:
+            new_text += letter
     return new_text
 
 def _reposition(current_position, shift, direction):
@@ -40,14 +43,7 @@ while go == "yes":
         print("Invalide encryption option, must be 'encode' or 'decode'")
     go = input("Would you like to keep encrypting messages? Type: 'yes' or 'no'\n")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
-
-#TODO-2: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-
-#TODO-3: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-#Hint: Think about how you can use the modulus (%).
+#TODO-1: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
