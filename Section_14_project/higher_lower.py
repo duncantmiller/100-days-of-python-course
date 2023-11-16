0e8107fa709b8214d86c8dd7cc55d336c45fda6f
Duncan Miller <dmiller@heatspring.com>
2023-11-16 14:01:35 -0800

    refactor printing into methods

--- Section_14_project/higher_lower.py
+++ Section_14_project/higher_lower.py
@@ -11,16 +11,25 @@ print("Welcome to the higher lower game. I will give you two choices and you hav
 def follower_count(choice):
     return choice["follower_count"]
 
+def print_name(choice):
+    print(f"Name: {choice['name']}")
+
+def print_description(choice):
+    print(f"Description: {choice['description']}")
+
+def print_followers(choice):
+    print(f"Followers: {follower_count(choices[0])}")
+
 keep_playing = True
 while keep_playing:
     choices = random.choices(game_data.data, k=2)
     print(choices)
-    print(f"Name: {choices[0]['name']}")
-    print(f"Description: {choices[0]['description']}")
-    print(f"Followers: {follower_count(choices[0])}")
+    print_name(choices[0])
+    print_description(choices[0])
+    print_followers(choices[0])
     print(art.vs)
-    print(f"Name: {choices[1]['name']}")
-    print(f"Description: {choices[1]['description']}")
+    print_name(choices[1])
+    print_description(choices[1])
     guess = input("Higher type 'h' or lower type 'l'?:\n")
     if guess == "h":
         if follower_count(choices[0]) < follower_count(choices[1]):
