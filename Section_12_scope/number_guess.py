d1b46f943113eb78a6eb74a497aa9c03cb62a897
Duncan Miller <dmiller@heatspring.com>
2023-11-16 13:30:33 -0800

    refactor number guess using methods

--- Section_12_scope/number_guess.py
+++ Section_12_scope/number_guess.py
@@ -1,17 +1,13 @@
 import random
-hard_chances = 5
-easy_chances = 10
+HARD_CHOICES = 5
+EASY_CHOICES = 10
 
-print("Welcome to the number guessing game")
-difficulty = input("do you want to play hard or easy? Type 'hard' or 'easy':\n")
-chances = easy_chances if difficulty == "easy" else hard_chances
-chances_left = chances
-picked_number = random.randint(1, 101)
-print("You will be guessing the number I have picked between 1 and 100.")
-keep_playing = True
-while chances_left > 0 and keep_playing:
-    print(f"You have {chances_left} guesses left.")
-    guess = int(input("Enter your guess:\n"))
+def decrement_chances(chances):
+    chances -= 1
+    return chances
+
+def check_guess(guess, picked_number):
+    keep_playing = True
     if guess == picked_number:
         keep_playing = False
         print(f"You win! The number was {picked_number}.")
@@ -19,12 +15,26 @@ while chances_left > 0 and keep_playing:
         print("Sorry too high.")
     else:
         print("Sorry too low.")
-    chances_left -= 1
+    return keep_playing
 
-if keep_playing:
-    print(f"You ran out of guesses! You lost. The number was {picked_number}.")
+def set_difficulty(difficulty):
+    chances = EASY_CHOICES if difficulty == "easy" else HARD_CHOICES
+    return chances
 
+def play():
+    difficulty = input("do you want to play hard or easy? Type 'hard' or 'easy':\n")
+    chances_left = set_difficulty(difficulty)
+    picked_number = random.randint(1, 100)
+    print("You will be guessing the number I have picked between 1 and 100.")
+    keep_playing = True
+    while chances_left > 0 and keep_playing:
+        print(f"You have {chances_left} guesses left.")
+        guess = int(input("Enter your guess:\n"))
+        keep_playing = check_guess(guess, picked_number)
+        chances_left = decrement_chances(chances_left)
 
+    if keep_playing:
+        print(f"You ran out of guesses! You lost. The number was {picked_number}.")
 
-
-
+print("Welcome to the number guessing game")
+play()
