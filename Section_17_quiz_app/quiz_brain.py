a716f58ca2463cc73602a3bf97bc3d30d683dab3
Duncan Miller <dmiller@heatspring.com>
2023-11-17 15:31:03 -0800

    track score

--- Section_17_quiz_app/quiz_brain.py
+++ Section_17_quiz_app/quiz_brain.py
@@ -2,9 +2,16 @@ class QuizBrain:
     def __init__(self, questions):
         self.question_number = 0
         self.questions_list = questions
+        self.correct_answers = 0
+
+    def question_total(self):
+        return len(self.questions_list)
 
     def increment_question_number(self):
         self.question_number += 1
+
+    def increment_correct_answers(self):
+        self.correct_answers += 1
 
     def human_question_number(self):
         return int(self.question_number) + 1
@@ -12,7 +19,17 @@ class QuizBrain:
     def still_has_questions(self):
         return self.question_number < len(self.questions_list)
 
+    def current_question(self):
+        return self.questions_list[self.question_number]
+
     def next_question(self):
-        question = self.questions_list[self.question_number]
-        input(f"Q.{self.human_question_number()} {question.text} (True/False): ")
+        answer = input(
+            f"Q.{self.human_question_number()} {self.current_question().text} (True/False): "
+        )
+        if self.check_answer(answer):
+            self.increment_correct_answers()
         self.increment_question_number()
+
+    def check_answer(self, answer):
+        question = self.current_question()
+        return answer == question.answer
