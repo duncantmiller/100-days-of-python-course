3f4646fc0107903c65ebf81c881edf3fa306df74
Duncan Miller <dmiller@heatspring.com>
2023-11-17 13:16:51 -0800

    fixing tab size

--- Section_16_oop/coffee_maker/main.py
+++ Section_16_oop/coffee_maker/main.py
@@ -8,16 +8,16 @@ money_machine = MoneyMachine()
 
 
 def new_order():
-  print("\nWelcome to the coffee machine.")
-  order = input(f"Please place your order ({menu.get_items()})\n")
-  if order == "report":
-      coffee_maker.report()
-      new_order()
-  elif order == "shutdown":
-      print("Goodbye.")
-  else:
-      drink = False
-      while not drink:
-        drink = menu.find_drink(order)
-      new_order()
+    print("\nWelcome to the coffee machine.")
+    order = input(f"Please place your order ({menu.get_items()})\n")
+    if order == "report":
+        coffee_maker.report()
+        new_order()
+    elif order == "shutdown":
+        print("Goodbye.")
+    else:
+        drink = False
+        while not drink:
+          drink = menu.find_drink(order)
+        new_order()
 new_order()
