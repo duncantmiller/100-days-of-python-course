3db475e729222392a1b0e48e65151079853a3843
Duncan Miller <dmiller@heatspring.com>
2023-11-17 13:32:00 -0800

    refactor process order into own methods

--- Section_16_oop/coffee_maker/main.py
+++ Section_16_oop/coffee_maker/main.py
@@ -10,6 +10,17 @@ def format_dollars(amount):
     """returns a string formatted in dollars $xx.xx"""
     return f"${amount:,.2f}"
 
+def collect_funds_and_make(drink):
+    if money_machine.make_payment(drink.cost):
+        coffee_maker.make_coffee(drink)
+
+def process_order(drink):
+    if coffee_maker.is_resource_sufficient(drink):
+        print(f"A latte costs {format_dollars(drink.cost)}")
+        collect_funds_and_make(drink)
+    else:
+        print(f"We can't make a {drink.name}. Please try again.")
+
 def new_order():
     print("\nWelcome to the coffee machine.")
     order = input(f"Please place your order ({menu.get_items()})\n")
@@ -22,11 +33,7 @@ def new_order():
         drink = False
         while not drink:
           drink = menu.find_drink(order)
-        if coffee_maker.is_resource_sufficient(drink):
-            print(f"A latte costs {format_dollars(drink.cost)}")
-            if money_machine.make_payment(drink.cost):
-                coffee_maker.make_coffee(drink)
-        else:
-            print(f"We can't make a {drink.name}. Please try again.")
+        process_order(drink)
         new_order()
+
 new_order()
