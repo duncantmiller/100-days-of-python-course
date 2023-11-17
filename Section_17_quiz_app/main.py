a23abf7be65838affc176448e0db633dcb5eea6c
Duncan Miller <dmiller@heatspring.com>
2023-11-17 15:59:24 -0800

    adding documentation docstrings

--- Section_17_quiz_app/main.py
+++ Section_17_quiz_app/main.py
@@ -1,12 +1,12 @@
+"""import Question, question data and QuizBrain"""
+
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
-    text = item["text"]
-    answer = item["answer"]
-    question = Question(text, answer)
+    question = Question(item["text"], item["answer"])
     question_bank.append(question)
 
 quiz = QuizBrain(question_bank)
