4ed80e1e9261c14428775d623de2c507d480c5cb
Duncan Miller <dmiller@heatspring.com>
2023-11-17 15:13:24 -0800

    quiz asks all the questions

--- Section_17_quiz_app/quiz_brain.py
+++ Section_17_quiz_app/quiz_brain.py
@@ -6,6 +6,13 @@ class QuizBrain:
     def increment_question_number(self):
         self.question_number += 1
 
+    def human_question_number(self):
+        return int(self.question_number) + 1
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
+
     def next_question(self):
         question = self.questions_list[self.question_number]
-        input(f"Q.{self.question_number} {question.text} (True/False)")
+        input(f"Q.{self.human_question_number()} {question.text} (True/False): ")
+        self.increment_question_number()
