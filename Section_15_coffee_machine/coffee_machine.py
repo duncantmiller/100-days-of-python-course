0f65df27846eb2272620f05b7e47873a47a51e49
Duncan Miller <dmiller@heatspring.com>
2023-11-16 16:42:32 -0800

    track resource use through orders

--- Section_15_coffee_machine/coffee_machine.py
+++ Section_15_coffee_machine/coffee_machine.py
@@ -24,12 +24,6 @@ MENU = {
     }
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
 def get_price(key):
     """retrieves the cost from the menu dictionary"""
     return MENU[key]["cost"]
@@ -50,32 +44,61 @@ def calculate_change(cost, paid):
     """calculates the change due"""
     return paid - cost
 
-def print_report():
+def print_report(resources):
     """Prints a report of the resources available"""
     for resource, amount in resources.items():
         print(f"{resource}: {amount}")
 
+def are_resources_available_for(key, resources):
+    """return true if there are enough ingredients else return false"""
+    for ingredient in MENU[key]["ingredients"]:
+        if MENU[key]["ingredients"][ingredient] > resources[ingredient]:
+            return False
+    return True
+
+def update_resources_for(key, resources):
+    """reduces resources by the proper amounts for the order"""
+    for ingredient in MENU[key]["ingredients"]:
+        resources[ingredient] -= MENU[key]["ingredients"][ingredient]
+    return resources
+
 def coffee_machine():
     """Makes your coffee"""
+    resources = {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+    }
     order = input("Please place your order espresso/latte/cappuccino:\n")
     if order == "report":
-        print_report()
+        print_report(resources)
+        coffee_machine()
+    elif order == "shutdown":
+        print("Goodbye.")
     else:
-        order_cost = get_price(order)
-        collect_money = True
-        while collect_money:
-            print(f"The cost of a {order} is {format_dollars(order_cost)}")
-            pennies_count = int(input("How many pennies: "))
-            nickels_count = int(input("How many nickels: "))
-            dimes_count = int(input("How many dimes: "))
-            quarters_count = int(input("How many quarters: "))
-            amount_paid = sum_currency(pennies_count, nickels_count, dimes_count, quarters_count)
-            if has_paid_enough(order_cost, amount_paid):
-                collect_money = False
-            else:
-                print(f"You only paid {format_dollars(amount_paid)}. I'm returning your money"
-                    " please try again")
-        change = calculate_change(order_cost, amount_paid)
-        print(f"Your change is: {format_dollars(change)}")
+        if are_resources_available_for(order, resources):
+            order_cost = get_price(order)
+            collect_money = True
+            while collect_money:
+                print(f"The cost of a {order} is {format_dollars(order_cost)}")
+                pennies_count = int(input("How many pennies: "))
+                nickels_count = int(input("How many nickels: "))
+                dimes_count = int(input("How many dimes: "))
+                quarters_count = int(input("How many quarters: "))
+                amount_paid = sum_currency(
+                    pennies_count, nickels_count, dimes_count, quarters_count
+                )
+                if has_paid_enough(order_cost, amount_paid):
+                    collect_money = False
+                else:
+                    print(f"You only paid {format_dollars(amount_paid)}. I'm returning your money"
+                        " please try again")
+            change = calculate_change(order_cost, amount_paid)
+            resources = update_resources_for(order, resources)
+            print(f"Your change is: {format_dollars(change)}")
+            print(f"Here is your {order}. Enjoy!\n")
+        else:
+            print(f"Sorry I don't have enough ingredients for a {order}. Please try again.\n")
+        coffee_machine()
 
 coffee_machine()
