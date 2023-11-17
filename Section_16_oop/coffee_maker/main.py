58fb81d7440b7fd4b7483098b9aa1b794202c218
Duncan Miller <dmiller@heatspring.com>
2023-11-17 12:08:27 -0800

    check if menu item exists

--- Section_16_oop/coffee_maker/main.py
+++ Section_16_oop/coffee_maker/main.py
@@ -1,3 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+print("Welcome to the coffee machine.")
+drink = False
+while not drink:
+  order = input(f"Please place your order ({menu.get_items()})\n")
+  drink = menu.find_drink(order)
+
