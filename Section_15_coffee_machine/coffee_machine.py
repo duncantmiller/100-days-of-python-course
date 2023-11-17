553cae90dd37807745ad3bd1958945548268299d
Duncan Miller <dmiller@heatspring.com>
2023-11-16 16:20:52 -0800

    print a report of resources

--- Section_15_coffee_machine/coffee_machine.py
+++ Section_15_coffee_machine/coffee_machine.py
@@ -50,24 +50,32 @@ def calculate_change(cost, paid):
     """calculates the change due"""
     return paid - cost
 
+def print_report():
+    """Prints a report of the resources available"""
+    for resource, amount in resources.items():
+        print(f"{resource}: {amount}")
+
 def coffee_machine():
     """Makes your coffee"""
     order = input("Please place your order espresso/latte/cappuccino:\n")
-    order_cost = get_price(order)
-    collect_money = True
-    while collect_money:
-        print(f"The cost of a {order} is {format_dollars(order_cost)}")
-        pennies_count = int(input("How many pennies: "))
-        nickels_count = int(input("How many nickels: "))
-        dimes_count = int(input("How many dimes: "))
-        quarters_count = int(input("How many quarters: "))
-        amount_paid = sum_currency(pennies_count, nickels_count, dimes_count, quarters_count)
-        if has_paid_enough(order_cost, amount_paid):
-            collect_money = False
-        else:
-            print(f"You only paid {format_dollars(amount_paid)}. I'm returning your money please"
-                " try again")
-    change = calculate_change(order_cost, amount_paid)
-    print(f"Your change is: {format_dollars(change)}")
+    if order == "report":
+        print_report()
+    else:
+        order_cost = get_price(order)
+        collect_money = True
+        while collect_money:
+            print(f"The cost of a {order} is {format_dollars(order_cost)}")
+            pennies_count = int(input("How many pennies: "))
+            nickels_count = int(input("How many nickels: "))
+            dimes_count = int(input("How many dimes: "))
+            quarters_count = int(input("How many quarters: "))
+            amount_paid = sum_currency(pennies_count, nickels_count, dimes_count, quarters_count)
+            if has_paid_enough(order_cost, amount_paid):
+                collect_money = False
+            else:
+                print(f"You only paid {format_dollars(amount_paid)}. I'm returning your money"
+                    " please try again")
+        change = calculate_change(order_cost, amount_paid)
+        print(f"Your change is: {format_dollars(change)}")
 
 coffee_machine()
