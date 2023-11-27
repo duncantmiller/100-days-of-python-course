538588461ccc037db90eb93db7a055f10c08ed43
Duncan Miller <dmiller@heatspring.com>
2023-11-27 12:57:46 -0800

    three turtles

--- Section_20_animation_coordinates/snake_game.py
+++ Section_20_animation_coordinates/snake_game.py
@@ -6,8 +6,21 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Field's snake game")
 
-field = Turtle()
-field.shape("square")
-field.color("white")
+turtles = []
+
+def add_turtle():
+    turtle = Turtle()
+    turtle.shape("square")
+    turtle.color("white")
+    if len(turtles) > 0:
+        last_coordinates = turtles[-1].position()
+        new_coordinates = (last_coordinates[0] - 20, last_coordinates[1])
+    else:
+        new_coordinates = (0, 0)
+    turtle.setpos(new_coordinates)
+    turtles.append(turtle)
+
+for _ in range(3):
+    add_turtle()
 
 screen.exitonclick()
