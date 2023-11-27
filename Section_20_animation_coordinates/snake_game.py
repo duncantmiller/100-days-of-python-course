05c53b3823454c794d705defacf6172c93bde3c3
Duncan Miller <dmiller@heatspring.com>
2023-11-27 12:45:00 -0800

    one turtle

--- Section_20_animation_coordinates/snake_game.py
+++ Section_20_animation_coordinates/snake_game.py
@@ -3,9 +3,11 @@ from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.gbcolor("black")
+screen.bgcolor("black")
 screen.title("Field's snake game")
 
-
+field = Turtle()
+field.shape("square")
+field.color("white")
 
 screen.exitonclick()
