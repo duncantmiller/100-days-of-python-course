e2b473a1812cb3674f44a72bc62062c7142e1b2d
Duncan Miller <dmiller@heatspring.com>
2023-11-27 13:37:43 -0800

    add snake class

---
+++ Section_20_animation_coordinates/snake.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+class Snake:
+    """Snake class"""
+
+    def __init__(self):
+        self.links = []
+        self.create_snake()
+
+    def create_snake(self):
+        for _ in range(3):
+            self.add_link()
+
+    def add_link(self):
+        """adds a new snake link"""
+        link = Turtle()
+        link.shape("square")
+        link.color("white")
+        links = self.links
+        if len(links) > 0:
+            last_coordinates = links[-1].position()
+            new_coordinates = (last_coordinates[0] - 20, last_coordinates[1])
+        else:
+            new_coordinates = (0, 0)
+        link.penup()
+        link.setpos(new_coordinates)
+        self.links.append(link)
+
+    def move(self):
+        """moves all the links to the next forward position"""
+        links = self.links
+        for link_number in range(len(links) - 1, 0, -1):
+            last_coordinates = links[link_number - 1].position()
+            links[link_number].goto(last_coordinates)
+        links[0].forward(20)
--- Section_20_animation_coordinates/snake_game.py
+++ Section_20_animation_coordinates/snake_game.py
@@ -1,5 +1,6 @@
 """turtle graphics"""
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,31 +9,12 @@ screen.bgcolor("black")
 screen.title("Field's snake game")
 screen.tracer(0)
 
-turtles = []
-
-def add_turtle():
-    turtle = Turtle()
-    turtle.shape("square")
-    turtle.color("white")
-    if len(turtles) > 0:
-        last_coordinates = turtles[-1].position()
-        new_coordinates = (last_coordinates[0] - 20, last_coordinates[1])
-    else:
-        new_coordinates = (0, 0)
-    turtle.penup()
-    turtle.setpos(new_coordinates)
-    turtles.append(turtle)
-
-for _ in range(3):
-    add_turtle()
+snake = Snake()
 
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-    for turtle_number in range(len(turtles) - 1, 0, -1):
-        last_coordinates = turtles[turtle_number - 1].position()
-        turtles[turtle_number].goto(last_coordinates)
-    turtles[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
