8ffa7b2a41a622492bfd2f245afc17ca44bc7dcc
Duncan Miller <dmiller@heatspring.com>
2023-11-27 12:40:58 -0800

    starting snake game

---
+++ Section_20_animation_coordinates/snake_game.py
@@ -0,0 +1,11 @@
+"""turtle graphics"""
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.gbcolor("black")
+screen.title("Field's snake game")
+
+
+
+screen.exitonclick()
