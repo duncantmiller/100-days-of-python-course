d6da45d282e11d9e5814d8665931f39cc5607a9f
Duncan Miller <dmiller@heatspring.com>
2023-11-28 14:22:07 -0800

    adding scoreboard

--- Section_23_capstone_turtle_crossing/scoreboard.py
+++ Section_23_capstone_turtle_crossing/scoreboard.py
@@ -1,4 +1,29 @@
+"""import turtle"""
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    """scoreboard class"""
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.setpos(-200, 250)
+        self._print_level()
+
+    def increment_level(self):
+        """increment level"""
+        self.level += 1
+        self._print_level()
+
+    def print_game_over(self):
+        """Prints game over"""
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
+
+    def _print_level(self):
+        """Prints the score"""
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
