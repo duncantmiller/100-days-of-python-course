e631670446a98518c8c506907ed4ceb13e842f2c
Duncan Miller <dmiller@heatspring.com>
2023-11-28 16:36:04 -0800

    use pandas mean method

--- Section_25_csv_pandas/main.py
+++ Section_25_csv_pandas/main.py
@@ -3,5 +3,4 @@ import pandas
 data = pandas.read_csv("weather_data.csv")
 temperatures = data["temp"].to_list()
 print(temperatures)
-average_temp = sum(temperatures) / len(temperatures)
-print(f"average temperature: {int(average_temp)}")
+print(f"average temperature: {int(data['temp'].mean())}")
