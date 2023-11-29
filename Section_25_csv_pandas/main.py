f9e36c4cdea35a22844a4b0ff6c99b4cdc258b0b
Duncan Miller <dmiller@heatspring.com>
2023-11-28 16:27:39 -0800

    use pandas to read csv instead

--- Section_25_csv_pandas/main.py
+++ Section_25_csv_pandas/main.py
@@ -1,10 +1,5 @@
-import csv
+import pandas
 
-with open("weather_data.csv") as file:
-    weather_data = csv.reader(file)
-    temperatures = []
-    for row in weather_data:
-        temperature = row[1]
-        if temperature != "temp":
-            temperatures.append(int(temperature))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+temperatures = data["temp"].to_list()
+print(temperatures)
