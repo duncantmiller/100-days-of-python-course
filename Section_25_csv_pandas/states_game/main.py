928c0f09755bfb93dbffc8f3c94b123ed7c4cbf0
Duncan Miller <dmiller@heatspring.com>
2023-11-29 15:29:13 -0800

    store correct guesses in list

--- Section_25_csv_pandas/states_game/main.py
+++ Section_25_csv_pandas/states_game/main.py
@@ -13,6 +13,7 @@ writer.penup()
 
 states_data = pandas.read_csv("50_states.csv")
 score = 0
+correct_guesses = []
 is_game_on = True
 while is_game_on:
     answer = screen.textinput(
@@ -21,6 +22,7 @@ while is_game_on:
     data = states_data[states_data.state == answer]
     if len(data["x"]) > 0:
         score += 1
+        correct_guesses.append(answer)
         writer.goto(int(data["x"]), int(data["y"]))
         writer.write(answer, move=False, align='center', font=('Arial', 8, 'normal'))
 
