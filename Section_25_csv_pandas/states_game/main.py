8864130fe0e52dd5d47911be62f3337a622b05b2
Duncan Miller <dmiller@heatspring.com>
2023-11-29 15:48:49 -0800

    add a csv of missing states

--- Section_25_csv_pandas/states_game/main.py
+++ Section_25_csv_pandas/states_game/main.py
@@ -14,6 +14,7 @@ writer.penup()
 states_data = pandas.read_csv("50_states.csv")
 score = 0
 correct_guesses = []
+unguessed_states = states_data.state.to_list()
 while len(correct_guesses) < 50:
     answer = screen.textinput(
         title=f"Score: {score}/50: Guess the State", prompt="What's another state name?"
@@ -21,10 +22,13 @@ while len(correct_guesses) < 50:
     if answer == "Exit":
         all_answers = pandas.DataFrame(correct_guesses, columns=['Correct Guesses'])
         all_answers.to_csv("correct_answers.csv")
+        missing_data = pandas.DataFrame(unguessed_states, columns=['Missing States'])
+        missing_data.to_csv("missing_states.csv")
         break
     data = states_data[states_data.state == answer]
     if len(data["x"]) > 0:
         score += 1
+        unguessed_states.remove(answer)
         correct_guesses.append(answer)
         writer.goto(int(data["x"]), int(data["y"]))
         writer.write(answer, move=False, align='center', font=('Arial', 8, 'normal'))
