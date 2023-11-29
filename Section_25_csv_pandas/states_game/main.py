67244cb12aa2305d77230eb31f8c5fa66f9aa8dc
Duncan Miller <dmiller@heatspring.com>
2023-11-29 15:41:32 -0800

    write correct answers to csv on exit

--- Section_25_csv_pandas/states_game/main.py
+++ Section_25_csv_pandas/states_game/main.py
@@ -14,15 +14,17 @@ writer.penup()
 states_data = pandas.read_csv("50_states.csv")
 score = 0
 correct_guesses = []
-while len(correct_guesses < 50):
+while len(correct_guesses) < 50:
     answer = screen.textinput(
         title=f"Score: {score}/50: Guess the State", prompt="What's another state name?"
     ).title()
+    if answer == "Exit":
+        all_answers = pandas.DataFrame(correct_guesses, columns=['Correct Guesses'])
+        all_answers.to_csv("correct_answers.csv")
+        break
     data = states_data[states_data.state == answer]
     if len(data["x"]) > 0:
         score += 1
         correct_guesses.append(answer)
         writer.goto(int(data["x"]), int(data["y"]))
         writer.write(answer, move=False, align='center', font=('Arial', 8, 'normal'))
-
-turtle.mainloop()
