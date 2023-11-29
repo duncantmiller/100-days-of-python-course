c0eef69572c117c1bc7ef5192f413cc624c90212
Duncan Miller <dmiller@heatspring.com>
2023-11-28 16:32:58 -0800

    print average temperature

--- Section_25_csv_pandas/main.py
+++ Section_25_csv_pandas/main.py
@@ -3,3 +3,5 @@ import pandas
 data = pandas.read_csv("weather_data.csv")
 temperatures = data["temp"].to_list()
 print(temperatures)
+average_temp = sum(temperatures) / len(temperatures)
+print(f"average temperature: {int(average_temp)}")
