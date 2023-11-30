f9aac09f0a5918b0752ed9d683ea5be1a884c6b0
Duncan Miller <dmiller@heatspring.com>
2023-11-30 11:03:23 -0800

    filter passing scores

--- Section_26_comprehension/dictionary/practice.py
+++ Section_26_comprehension/dictionary/practice.py
@@ -3,4 +3,7 @@ names = "Alex Beth Caroline Dave Ellen Mike".split()
 
 scores = {name:random.randint(1,100) for name in names}
 
+passed_scores = {record for record in scores if scores[record] > 75}
+
 print(scores)
+print(passed_scores)
