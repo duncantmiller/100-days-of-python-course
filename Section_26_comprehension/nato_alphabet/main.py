145e6ca2f115a1cea160454d3c2ff75d25fc7b83
Duncan Miller <dmiller@heatspring.com>
2023-11-30 11:35:43 -0800

    translate spelling

--- Section_26_comprehension/nato_alphabet/main.py
+++ Section_26_comprehension/nato_alphabet/main.py
@@ -1,7 +1,7 @@
 import pandas
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#{"A": "Alfa", "B": "Bravo"}
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
@@ -10,5 +10,11 @@ for (index, row) in nato_df.iterrows():
     nato_dictionary[row.letter] = row.code
 
 print(nato_dictionary)
+
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+word = input("Type a word to see it spelled in Nato phonetics:\n").upper()
+
+codes = [nato_dictionary[letter] for letter in word]
+
+print(codes)
