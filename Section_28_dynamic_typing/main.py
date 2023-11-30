59193c161d6c1f4bdf00067126460295b374afe1
Duncan Miller <dmiller@heatspring.com>
2023-11-30 14:29:07 -0800

    set up window

--- Section_28_dynamic_typing/main.py
+++ Section_28_dynamic_typing/main.py
@@ -1,5 +1,5 @@
+import tkinter as tk
 
-# ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -9,10 +9,19 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+canvas = tk.Canvas(width=200, height=224)
+
+image = tk.PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=image)
+canvas.pack()
+window.mainloop()
