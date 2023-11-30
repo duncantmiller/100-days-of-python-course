8f24722d264f0abcba8778facca532360b00676d
Duncan Miller <dmiller@heatspring.com>
2023-11-30 14:41:55 -0800

    ui buttons and labels

--- Section_28_dynamic_typing/main.py
+++ Section_28_dynamic_typing/main.py
@@ -21,8 +21,20 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
+timer_label = tk.Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 60))
+timer_label.grid(column=1, row=0)
 image = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = tk.Button(text="Start", bg=YELLOW, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = tk.Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_label = tk.Label(text="✓", bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
+
 window.mainloop()
