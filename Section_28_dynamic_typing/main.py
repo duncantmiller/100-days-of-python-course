a2f8db6a1bc54779f331bb565387345a18931167
Duncan Miller <dmiller@heatspring.com>
2023-11-30 14:34:06 -0800

    background and timer

--- Section_28_dynamic_typing/main.py
+++ Section_28_dynamic_typing/main.py
@@ -18,10 +18,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
-canvas = tk.Canvas(width=200, height=224)
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
 image = tk.PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=image)
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 window.mainloop()
