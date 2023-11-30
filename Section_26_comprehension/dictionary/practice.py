b46ce360685fed0a831840803b18d414aac2a16f
Duncan Miller <dmiller@heatspring.com>
2023-11-30 11:05:16 -0800

    explicitly add key value pair

--- Section_26_comprehension/dictionary/practice.py
+++ Section_26_comprehension/dictionary/practice.py
@@ -3,7 +3,7 @@ names = "Alex Beth Caroline Dave Ellen Mike".split()
 
 scores = {name:random.randint(1,100) for name in names}
 
-passed_scores = {record for record in scores if scores[record] > 75}
+passed_scores = {key:value for (key, value) in scores.items() if scores[key] > 75}
 
 print(scores)
 print(passed_scores)
