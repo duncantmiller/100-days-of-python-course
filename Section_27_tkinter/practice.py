a6aec8d411cea36f8ea95ff4c96303e8341f5364
Duncan Miller <dmiller@heatspring.com>
2023-11-30 13:36:03 -0800

    add a button

--- Section_27_tkinter/practice.py
+++ Section_27_tkinter/practice.py
@@ -2,16 +2,11 @@ import tkinter
 
 window = tkinter.Tk()
 
-def add(*args):
-    total = 0
-    for number in args:
-        total += number
-    return total
-
-print(add(1, 2, 3, 4))
 window.title("A GUI Program")
 window.minsize(width=500, height=300)
 label = tkinter.Label(text="Label 1", font=("Arial", 24, "bold"))
 label.pack()
+button = tkinter.Button(text="click me")
+button.pack()
 
 window.mainloop()
