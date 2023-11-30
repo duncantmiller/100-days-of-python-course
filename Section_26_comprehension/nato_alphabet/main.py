628ed679ac0742b4a4ae6be9955c20853cc8a9da
Duncan Miller <dmiller@heatspring.com>
2023-11-30 11:31:08 -0800

    print dictionary

--- Section_26_comprehension/nato_alphabet/main.py
+++ Section_26_comprehension/nato_alphabet/main.py
@@ -1,5 +1,14 @@
+import pandas
+
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_dictionary = {}
+for (index, row) in nato_df.iterrows():
+    nato_dictionary[row.letter] = row.code
+
+print(nato_dictionary)
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
