d7ab49b0951cf62d9de0cbc71b6c4f05975c2c0b
Duncan Miller <dmiller@heatspring.com>
2023-12-01 11:18:23 -0800

    set up a canvas with the image

--- Section_29_password_manager/main.py
+++ Section_29_password_manager/main.py
@@ -1,5 +1,15 @@
+import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+canvas = tk.Canvas(width=200, height=200)
+image = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=image)
+canvas.pack()
+window.mainloop()
