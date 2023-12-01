000d4057f3a4c5fa87170c281b6197a6b8328dd6
Duncan Miller <dmiller@heatspring.com>
2023-12-01 13:56:38 -0800

    add exception handling to nato project

--- Section_30_exception_handling/nato_alphabet_2/main.py
+++ Section_30_exception_handling/nato_alphabet_2/main.py
@@ -2,7 +2,16 @@ import pandas
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter:row.code for (index, row) in nato_df.iterrows()}
-word = input("Type a word to see it spelled in Nato phonetics:\n").upper()
-codes = [nato_dictionary[letter] for letter in word]
 
-print(codes)
+def generate_code():
+    """generate the phonetic code and handle non letters"""
+    word = input("Type a word to see it spelled in Nato phonetics:\n").upper()
+    try:
+        codes = [nato_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_code()
+    else:
+        print(codes)
+
+generate_code()
