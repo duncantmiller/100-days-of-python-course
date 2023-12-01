7abdc66962274d828fb6e9212df0ebd70d073bf7
Duncan Miller <dmiller@heatspring.com>
2023-11-30 17:38:04 -0800

    add minutes and seconds to timer

--- Section_28_dynamic_typing/main.py
+++ Section_28_dynamic_typing/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -18,13 +19,18 @@ canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+def start_timer():
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-def count_down(count=5):
+def count_down(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
         count -= 1
         window.after(1000, count_down, count)
-        canvas.itemconfig(timer_text, text=count)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -36,7 +42,7 @@ canvas.create_image(100, 112, image=image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = tk.Button(text="Start", bg=YELLOW, highlightthickness=0, command=count_down)
+start_button = tk.Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = tk.Button(text="Reset", bg=YELLOW, highlightthickness=0)
