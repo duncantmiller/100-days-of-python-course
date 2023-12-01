1c0e8b6f4e2dbbe39b520895905273946c68edf0
Duncan Miller <dmiller@heatspring.com>
2023-11-30 17:28:43 -0800

    add timer

--- Section_28_dynamic_typing/main.py
+++ Section_28_dynamic_typing/main.py
@@ -9,26 +9,34 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+def count_down(count=5):
+    if count > 0:
+        count -= 1
+        window.after(1000, count_down, count)
+        canvas.itemconfig(timer_text, text=count)
+
 # ---------------------------- UI SETUP ------------------------------- #
-window = tk.Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
-canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
 timer_label = tk.Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 60))
 timer_label.grid(column=1, row=0)
+timer_label.grid(column=1, row=0)
 image = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = tk.Button(text="Start", bg=YELLOW, highlightthickness=0)
+start_button = tk.Button(text="Start", bg=YELLOW, highlightthickness=0, command=count_down)
 start_button.grid(column=0, row=2)
 
 reset_button = tk.Button(text="Reset", bg=YELLOW, highlightthickness=0)
