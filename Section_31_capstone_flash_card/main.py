91aaeff73b5916836e546dbb7ef76ddf0f72754e
Duncan Miller <dmiller@heatspring.com>
2023-12-01 16:25:33 -0800

    add background and swap buttons

--- Section_31_capstone_flash_card/main.py
+++ Section_31_capstone_flash_card/main.py
@@ -4,7 +4,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = tk.Tk()
 window.title("Flashcards")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = tk.Canvas(width=200, height=200)
 card_front_image = tk.PhotoImage(file="images/card_front.png")
 canvas.create_image(150, 100, image=card_front_image)
@@ -12,9 +12,9 @@ canvas.grid(column=1, row=0)
 
 correct_image = tk.PhotoImage(file="images/right.png")
 correct_button = tk.Button(image=correct_image, highlightthickness=0)
-correct_button.grid(column=0, row=2)
+correct_button.grid(column=2, row=2)
 incorrect_image = tk.PhotoImage(file="images/wrong.png")
 incorrect_button = tk.Button(image=incorrect_image, highlightthickness=0)
-incorrect_button.grid(column=2, row=2)
+incorrect_button.grid(column=0, row=2)
 
 tk.mainloop()
