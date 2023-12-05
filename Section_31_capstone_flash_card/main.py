8bd849314055d7f15f1bf9038fb8c5123eb8b9c7
Duncan Miller <dmiller@heatspring.com>
2023-12-05 14:09:42 -0800

    card flips and changes color when advancing

--- Section_31_capstone_flash_card/main.py
+++ Section_31_capstone_flash_card/main.py
@@ -3,6 +3,8 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+ENGLISH = "English"
+FRENCH = "French"
 
 def retrieve_words():
     """read the file contents and return a dictionary"""
@@ -15,32 +17,51 @@ def select_random_entry_from(dictionary):
     """get a random word combination"""
     return random.choice(dictionary)
 
-def french_word(entry):
-    """returns the french word in the entry"""
-    return entry["French"]
-
-def english_word(entry):
-    """returns the english word in the entry"""
-    return entry["English"]
-
-def update_word_text(word):
+def update_word_text(language, entry):
     """updates the word on the card"""
-    canvas.itemconfig(word_text, text=word)
+    canvas.itemconfig(word_text, text=entry[language])
 
 def update_language_text(language):
     """updates the language on the card"""
     canvas.itemconfig(language_text, text=language)
 
+def update_card_for(language):
+    """updates the the card color"""
+    if language == ENGLISH:
+        canvas.itemconfig(card_image, image=card_back_image)
+        canvas.itemconfig(language_text, fill="White")
+        canvas.itemconfig(word_text, fill="White")
+    else:
+        canvas.itemconfig(card_image, image=card_front_image)
+        canvas.itemconfig(language_text, fill="Black")
+        canvas.itemconfig(word_text, fill="Black")
+
 def next_word():
-    random_entry = select_random_entry_from(dictionary)
-    update_word_text(french_word(random_entry))
+    """set up the next word"""
+    window.after_cancel(timer)
+    entry = select_random_entry_from(dictionary)
+    update_word_text(FRENCH, entry)
+    update_language_text(FRENCH)
+    update_card_for(FRENCH)
+    start_timer(entry)
+
+def flip_card(language, entry):
+    """flips the card and updates the text"""
+    update_language_text(language)
+    update_word_text(language, entry)
+    update_card_for(language)
+
+def start_timer(entry):
+    """start the timer on the new word"""
+    return window.after(3000, flip_card, ENGLISH, entry)
 
 window = tk.Tk()
 window.title("Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = tk.Canvas(width=800, height=526)
 card_front_image = tk.PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = tk.PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(400, 263, image=card_front_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
@@ -54,9 +75,9 @@ incorrect_button = tk.Button(image=incorrect_image, highlightthickness=0, comman
 incorrect_button.grid(column=0, row=2)
 
 dictionary = retrieve_words()
-random_entry = select_random_entry_from(dictionary)
-update_word_text(french_word(random_entry))
+entry = select_random_entry_from(dictionary)
+update_word_text(FRENCH, entry)
 
-window.after(3000, next_word)
+timer = start_timer(entry)
 
 tk.mainloop()
