af7fa9142444aac4f1a335c78f2416cc5f26100f
Duncan Miller <dmiller@heatspring.com>
2023-12-07 09:51:59 -0800

    find tags and css

--- Section_45_web_scraping/main.py
+++ Section_45_web_scraping/main.py
@@ -11,3 +11,10 @@ tags = soup.find_all(name="a")
 
 for tag in tags:
     print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+company_url = soup.select_one(selector="p a")
+
+print(company_url)
