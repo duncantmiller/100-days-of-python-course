8f1301fd617b5df52b32c8dcabf77359c08dd4d5
Duncan Miller <dmiller@heatspring.com>
2023-12-07 11:27:07 -0800

    get the price from an amazon page

--- Section_48_selenium/main.py
+++ Section_48_selenium/main.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.amazon.com")
+driver.get("https://www.amazon.com/Barleans-Supplement-Softgels-Health-Flavored/dp/B002VLZ8AI")
+
+price_element = driver.find_element(By.XPATH, value='//*[@id="sns-base-price"]/div/span[1]/span[2]')
+print(price_element.text)
