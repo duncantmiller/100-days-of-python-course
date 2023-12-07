514b89c5a979aa561128667789fad6f6bcec29f1
Duncan Miller <dmiller@heatspring.com>
2023-12-07 10:29:53 -0800

    retrieve the data for the front page links

--- Section_45_web_scraping/hacker_news/main.py
+++ Section_45_web_scraping/hacker_news/main.py
@@ -6,4 +6,20 @@ content = BeautifulSoup(response.text, "html.parser")
 
 rows = content.find_all(name="tr", class_="athing")
 
-print(rows)
+front_page_links = []
+for row in rows:
+    title_id = row.get("id")
+    title_link = row.select_one("td.title span a")
+    points_span = content.select_one(f"span#score_{title_id}")
+    if points_span:
+        points_value = int(points_span.string.split(" ")[0])
+    else:
+        points_value = 0
+    row_data = {
+        "url": title_link.get("href"),
+        "title": title_link.string,
+        "points": points_value
+    }
+    front_page_links.append(row_data)
+
+print(front_page_links)
