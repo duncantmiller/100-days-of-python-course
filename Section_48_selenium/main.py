e759ada0f1ebce8fc9c94665dc648f29a63cba77
Duncan Miller <dmiller@heatspring.com>
2023-12-07 11:55:19 -0800

    fill in a form

--- Section_48_selenium/main.py
+++ Section_48_selenium/main.py
@@ -5,7 +5,11 @@ driver = webdriver.Chrome()
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-article_count.click()
+# article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# article_count.click()
 
-driver.find_element(By.LINK_TEXT, "View history").click()
+# driver.find_element(By.LINK_TEXT, "View history").click()
+
+search_box = driver.find_element(By.NAME, "search")
+search_box.send_keys("Python")
+driver.find_element(By.CLASS_NAME, "cdx-search-input__end-button").click()
