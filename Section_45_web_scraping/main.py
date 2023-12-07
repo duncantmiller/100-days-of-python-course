888dc746697e2aa90ac59aa6357fbbb253253a8b
Duncan Miller <dmiller@heatspring.com>
2023-12-07 09:48:06 -0800

    get the links

--- Section_45_web_scraping/main.py
+++ Section_45_web_scraping/main.py
@@ -7,3 +7,7 @@ soup = BeautifulSoup(contents, "html.parser")
 print(soup.title.string)
 print(soup.prettify())
 print(soup.find_all(name="p"))
+tags = soup.find_all(name="a")
+
+for tag in tags:
+    print(tag.get("href"))
