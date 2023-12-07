bbdb5b25a3706f7fec4a4a78b35b9f8a65524346
Duncan Miller <dmiller@heatspring.com>
2023-12-06 17:17:20 -0800

    adding docstrings

--- Section_33_api_requests/main.py
+++ Section_33_api_requests/main.py
@@ -6,17 +6,21 @@ MY_LATITUDE = 45.51481
 MY_LONGITUDE = -122.68427
 
 def iss_json():
+    """returns json of iss response"""
     iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
     iss_response.raise_for_status()
     return iss_response.json()
 
 def iss_latitude():
+    """iss latitude"""
     return float(iss_json()["iss_position"]["latitude"])
 
 def iss_longitude():
+    "iss longitude"
     return float(iss_json["iss_position"]["longitude"])
 
 def parameters():
+    """params for sun api"""
     return {
         "lat": MY_LATITUDE,
         "lng": MY_LONGITUDE,
@@ -24,20 +28,25 @@ def parameters():
     }
 
 def my_hour_of(value):
+    """gets the hour value from the sun response"""
     return int(sun_json()["results"][value].split("T")[1].split(":")[0])
 
 def sun_json():
+    """returns json of sun response"""
     sun_response = requests.get(url=f"http://api.sunrise-sunset.org/json?", params=parameters())
     sun_response.raise_for_status()
     return sun_response.json()
 
 def current_hour():
+    """current hour"""
     return datetime.now().hour
 
 def is_night():
+    """checks to see if it is currently after sunset or before sunrise"""
     return current_hour() > my_hour_of("sunset") and current_hour() < my_hour_of("sunrise")
 
 def is_iss_near():
+    """checks to see if the iss is within 5 degrees of our position"""
     abs(iss_latitude() - MY_LATITUDE) < 5 and abs(iss_longitude() - MY_LONGITUDE) < 5
 
 while True:
