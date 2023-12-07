ae33b16fbe2166af68e4021b7146560baf9355bf
Duncan Miller <dmiller@heatspring.com>
2023-12-07 09:47:04 -0800

    testing bs4 commands

--- Section_45_web_scraping/main.py
+++ Section_45_web_scraping/main.py
@@ -4,4 +4,6 @@ with open("website.html", "r") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup)
+print(soup.title.string)
+print(soup.prettify())
+print(soup.find_all(name="p"))
