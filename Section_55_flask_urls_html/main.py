4832deac86b73535599e21d109ad517a9ec5531c
Duncan Miller <dmiller@heatspring.com>
2023-12-07 14:02:31 -0800

    pass the kwargs to the h1 decorator

--- Section_55_flask_urls_html/main.py
+++ Section_55_flask_urls_html/main.py
@@ -3,13 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 def make_bold(function):
-    def wrapper():
+    def bold_wrapper():
         return f"<b>{function()}</b>"
-    return wrapper
+    return bold_wrapper
+
+def make_h1(function):
+    def h1_wrapper(**kwargs):
+        return f"<h1>{function(**kwargs)}</h1>"
+    return h1_wrapper
 
 @app.route('/name/<username>')
+@make_h1
 def hello(username):
-    return f"<h1>hello {username}</h1>"
+    return f"hello {username}"
 
 @app.route('/bye')
 @make_bold
