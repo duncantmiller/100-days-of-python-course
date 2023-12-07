60dcf2db47b2cfc31475f72ab8aa7bd2d19cb612
Duncan Miller <dmiller@heatspring.com>
2023-12-07 15:33:19 -0800

    display blog posts from api

--- Section_57_jinja/server.py
+++ Section_57_jinja/server.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+all_posts = posts.json()
 
 @app.route('/')
 def home():
     return render_template("index.html", num=1)
+
+@app.route('/blog')
+def blog_index():
+    return render_template("blog/index.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
