c23294bda3f99592f7e9e33fc37a3be1cdb891df
Duncan Miller <dmiller@heatspring.com>
2023-12-07 13:57:34 -0800

    add bold wrapper

--- Section_55_flask_urls_html/main.py
+++ Section_55_flask_urls_html/main.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
 @app.route('/name/<username>')
 def hello(username):
     return f"<h1>hello {username}</h1>"
+
+@app.route('/bye')
+@make_bold
+def bye():
+    return "Bye"
 
 if __name__ == "__main__":
     app.run(debug=True)
