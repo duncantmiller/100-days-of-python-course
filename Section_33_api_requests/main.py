7c4f9c33717a03db181aa5e4dee7c19389bdac87
Duncan Miller <dmiller@heatspring.com>
2023-12-06 16:30:20 -0800

    check if its overhead and send a message

--- Section_33_api_requests/main.py
+++ Section_33_api_requests/main.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import time
 
 MY_LATITUDE = 45.51481
 MY_LONGITUDE = -122.68427
@@ -8,9 +9,8 @@ iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
 iss_response.raise_for_status()
 
 iss_json = iss_response.json()
-print(iss_json)
-iss_latitude = iss_json["iss_position"]["latitude"]
-iss_longitude = iss_json["iss_position"]["longitude"]
+iss_latitude = float(iss_json["iss_position"]["latitude"])
+iss_longitude = float(iss_json["iss_position"]["longitude"])
 
 parameters = {
     "lat": MY_LATITUDE,
@@ -20,10 +20,14 @@ parameters = {
 
 sun_response = requests.get(url=f"http://api.sunrise-sunset.org/json?", params=parameters)
 sun_json = sun_response.json()
-print(sun_response.json())
+sunrise_hour = int(sun_json["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset_hour = int(sun_json["results"]["sunset"].split("T")[1].split(":")[0])
 
-sunrise = sun_json["results"]["sunrise"]
-sunset = sun_json["results"]["sunset"]
-
-print(sunrise.split("T")[1].split(":")[0])
-time_now = datetime.now()
+while True:
+    current_hour = datetime.now().hour
+    if current_hour > sunset_hour and current_hour < sunrise_hour:
+        if abs(iss_latitude - MY_LATITUDE) < 5 and abs(iss_longitude - MY_LONGITUDE) < 5:
+            print("Look up!")
+    else:
+        print("Don't look up.")
+    time.sleep(60)
