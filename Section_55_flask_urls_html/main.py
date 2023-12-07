9789e899c42bebae8a295320c05e171830adc11f
Duncan Miller <dmiller@heatspring.com>
2023-12-07 14:14:39 -0800

    check the guess at the url

--- Section_55_flask_urls_html/main.py
+++ Section_55_flask_urls_html/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
+
+random_number = random.randint(1, 10)
 
 def make_bold(function):
     def bold_wrapper():
@@ -12,6 +15,14 @@ def make_h1(function):
         return f"<h1>{function(**kwargs)}</h1>"
     return h1_wrapper
 
+def check_guess(function):
+    def guess_wrapper(**kwargs):
+        if kwargs["number"] == random_number:
+            return f"{function(**kwargs)} is right!"
+        else:
+            return f"Nope {function(**kwargs)}  is wrong try again."
+    return guess_wrapper
+
 @app.route('/name/<username>')
 @make_h1
 def hello(username):
@@ -21,6 +32,11 @@ def hello(username):
 @make_bold
 def bye():
     return "Bye"
+
+@app.route('/guess/<int:number>')
+@check_guess
+def guess(number):
+    return f"{number}"
 
 if __name__ == "__main__":
     app.run(debug=True)
