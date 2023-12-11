68b5ab4f6b90d05321434cd6aa638aedbe3ff809
Duncan Miller <dmiller@heatspring.com>
2023-12-11 13:34:41 -0800

    change string to datetime

--- Section_73_matplotlib/main.py
+++ Section_73_matplotlib/main.py
@@ -6,3 +6,8 @@ print(data_frame.head())
 print(data_frame.shape)
 print(data_frame.groupby('Tag').sum())
 print(data_frame.groupby('Tag').sum().idxmax())
+
+print(type(data_frame['Date'][1]))
+
+data_frame['Date'] = pandas.to_datetime(data_frame['Date'])
+print(type(data_frame['Date'][1]))
