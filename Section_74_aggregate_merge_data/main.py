839464a9cadd6b1c9a94167d93a7a6da0a399436
Duncan Miller <dmiller@heatspring.com>
2023-12-11 14:15:10 -0800

    print all the product names for the first year

--- Section_74_aggregate_merge_data/main.py
+++ Section_74_aggregate_merge_data/main.py
@@ -11,8 +11,10 @@ data_frame = pandas.read_csv('data/sets.csv')
 print(data_frame.head())
 # In which year were the first LEGO sets released and what were these sets called?
 
-print(data_frame.loc[data_frame['year'].idxmin()]['name'])
+rows = data_frame[data_frame['year'] == data_frame['year'].min()]
+print(rows['name'])
 
 # How many different products did the LEGO company sell in their first year of operation?
+
 
 # What are the top 5 LEGO sets with the most number of parts?
