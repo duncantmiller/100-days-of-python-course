149a8cce88025d1bcd9af978962efb4a6abfcd3d
Duncan Miller <dmiller@heatspring.com>
2023-12-12 13:09:48 -0800

    read in the themes.csv file

--- Section_74_aggregate_merge_data/sets_per_theme.py
+++ Section_74_aggregate_merge_data/sets_per_theme.py
@@ -1,7 +1,8 @@
 import pandas
 import matplotlib.pyplot as pyplot
 
-data_frame = pandas.read_csv('data/sets.csv')
+sets_data_frame = pandas.read_csv('data/sets.csv')
+themes_data_frame = pandas.read_csv('data/themes.csv')
 
-set_theme_count = data_frame['theme_id'].value_counts()
+set_theme_count = sets_data_frame['theme_id'].value_counts()
 print(set_theme_count)
