577f4ce780798517b774be20e36a39c5e61b369e
Duncan Miller <dmiller@heatspring.com>
2023-12-12 13:19:28 -0800

    merge the theme names dataframe

--- Section_74_aggregate_merge_data/sets_per_theme.py
+++ Section_74_aggregate_merge_data/sets_per_theme.py
@@ -4,5 +4,7 @@ import matplotlib.pyplot as pyplot
 sets_data_frame = pandas.read_csv('data/sets.csv')
 themes_data_frame = pandas.read_csv('data/themes.csv')
 
-set_theme_count = sets_data_frame['theme_id'].value_counts()
-print(set_theme_count)
+set_theme_count = pandas.DataFrame(sets_data_frame['theme_id'].value_counts())
+merged_data_frame = pandas.merge(set_theme_count, themes_data_frame, left_on='theme_id', right_on='id', how='inner')
+
+print(merged_data_frame)
