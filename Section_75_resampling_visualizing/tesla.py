1a950e4cd21e0337a96046d61080322221ba8f22
Duncan Miller <dmiller@heatspring.com>
2023-12-12 14:54:41 -0800

    add minor date ticks

--- Section_75_resampling_visualizing/tesla.py
+++ Section_75_resampling_visualizing/tesla.py
@@ -1,10 +1,15 @@
 import pandas
 import matplotlib.pyplot as pyplot
+import matplotlib.dates as mdates
 
 df_tesla = pandas.read_csv('TESLA Search Trend vs Price.csv')
 df_unemployment = pandas.read_csv('UE Benefits Search vs UE Rate 2004-20.csv')
 df_btc_price = pandas.read_csv('Daily Bitcoin Price.csv')
 df_btc_search = pandas.read_csv('Bitcoin Search Trend.csv')
+
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+years_formatted = mdates.DateFormatter('%Y')
 
 print(df_btc_price[df_btc_price.isna().any(axis=1)])
 print(df_btc_price.isna().sum())
@@ -27,6 +32,9 @@ pyplot.figure(figsize=(14, 9), dpi=120)
 pyplot.title('Tesla Web Search vs Price', fontsize=18)
 pyplot.xticks(fontsize=14, rotation=45)
 axis_1 = pyplot.gca()
+axis_1.xaxis.set_major_locator(years)
+axis_1.xaxis.set_minor_locator(months)
+axis_1.xaxis.set_major_formatter(years_formatted)
 axis_1.set_xlim([df_tesla['MONTH'].min(), df_tesla["MONTH"].max()])
 axis_1.plot(df_tesla['MONTH'], df_tesla['TSLA_WEB_SEARCH'], linewidth=3)
 axis_2 = axis_1.twinx()
