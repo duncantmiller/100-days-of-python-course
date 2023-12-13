999b84fb454181b5e906c0f999e4c0d9ebca0b98
Duncan Miller <dmiller@heatspring.com>
2023-12-13 09:53:11 -0800

    drop columns inplace and remove nan

--- Section_76_plotly/main.py
+++ Section_76_plotly/main.py
@@ -1,6 +1,8 @@
 import pandas
 
 df_apps = pandas.read_csv('apps.csv')
-df_apps.drop(columns=['Last_Updated', 'Android_Ver'])
+df_apps.drop(columns=['Last_Updated', 'Android_Ver'], inplace=True)
 print(df_apps.shape)
 print(df_apps.sample())
+df_apps_clean = df_apps.dropna()
+print(df_apps_clean.shape)
