b8c6133b0f1d66fbe68fbf31f810b4fe12a95eb3
Duncan Miller <dmiller@heatspring.com>
2023-12-13 09:56:34 -0800

    print sample to show columns

--- Section_76_plotly/main.py
+++ Section_76_plotly/main.py
@@ -5,4 +5,6 @@ df_apps.drop(columns=['Last_Updated', 'Android_Ver'], inplace=True)
 print(df_apps.shape)
 print(df_apps.sample())
 df_apps_clean = df_apps.dropna()
+print(df_apps.sample())
 print(df_apps_clean.shape)
+
