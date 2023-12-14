956d23dca1775b9da3278b2093fb6744b1ee1eb5
Duncan Miller <dmiller@heatspring.com>
2023-12-14 11:21:24 -0800

    vector operations

---
+++ Section_77_numpy/linear_alg.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+from PIL import Image
+
+v1 = np.array([4, 5, 2, 7])
+v2 = np.array([2, 1, 3, 3])
+print(v1 + v2)
+print(v1 * v2)
+print(v1 * 2)
