81eda109771029f87b263cbe04d2de203462ba3c
Duncan Miller <dmiller@heatspring.com>
2023-12-14 14:43:06 -0800

    donut chart of m vs f

--- Section_79_advanced_data_analysis/main.py
+++ Section_79_advanced_data_analysis/main.py
@@ -1,5 +1,6 @@
 import pandas
 import matplotlib.pyplot as pyplot
+import plotly.express as plotly
 import seaborn
 
 df = pandas.read_csv('nobel_prize_data.csv')
@@ -18,3 +19,13 @@ numerator = pandas.to_numeric(prize_share[0])
 denominator = pandas.to_numeric(prize_share[1])
 df['share_percent'] = numerator / denominator
 print(df.info())
+
+sex_data = df['sex'].value_counts()
+
+fig = plotly.pie(labels=sex_data.index,
+                 values=sex_data.values,
+                 title="Percentage M vs F",
+                 names=sex_data.index,
+                 hole=0.4)
+fig.update_traces(textposition='inside', textfont_size=15, textinfo='percent')
+fig.show()
