0e261daf5e8a9218d383b7f4f3ded477417a5ff4
Duncan Miller <dmiller@heatspring.com>
2023-12-14 14:30:36 -0800

    calculate a prize share from string

--- Section_79_advanced_data_analysis/main.py
+++ Section_79_advanced_data_analysis/main.py
@@ -9,3 +9,10 @@ print(df.describe())
 print(df[df.duplicated() == True])
 print(df[df.isna() == True])
 
+df['birth_date'] = pandas.to_datetime(df['birth_date'])
+print(df['prize_share'])
+prize_share = df['prize_share'].str.split("/", expand=True)
+numerator = pandas.to_numeric(prize_share[0])
+denominator = pandas.to_numeric(prize_share[1])
+df['share_percent'] = numerator / denominator
+print(df.info())
