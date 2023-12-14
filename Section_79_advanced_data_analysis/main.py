643921e374eb6746ff342d62b27b6c35d375d135
Duncan Miller <dmiller@heatspring.com>
2023-12-14 14:22:38 -0800

    look at data duplicates and nan

--- Section_79_advanced_data_analysis/main.py
+++ Section_79_advanced_data_analysis/main.py
@@ -4,3 +4,8 @@ import seaborn
 
 df = pandas.read_csv('nobel_prize_data.csv')
 print(df.shape)
+print(df.info())
+print(df.describe())
+print(df[df.duplicated() == True])
+print(df[df.isna() == True])
+
