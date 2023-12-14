1a762ecdd74cbf1a106dbbf6910611bdab7316c1
Duncan Miller <dmiller@heatspring.com>
2023-12-14 11:22:56 -0800

    use matmul for different shaped arrays

--- Section_77_numpy/linear_alg.py
+++ Section_77_numpy/linear_alg.py
@@ -7,3 +7,14 @@ v2 = np.array([2, 1, 3, 3])
 print(v1 + v2)
 print(v1 * v2)
 print(v1 * 2)
+
+a1 = np.array([[1, 3],
+               [0, 1],
+               [6, 2],
+               [9, 7]])
+
+b1 = np.array([[4, 1, 3],
+               [5, 8, 5]])
+
+print(a1 @ b1)
+print(np.matmul(a1, b1))
