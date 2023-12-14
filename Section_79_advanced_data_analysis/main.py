0aea3dd65857e88f79cfa24c71dd5952c5094d59
Duncan Miller <dmiller@heatspring.com>
2023-12-14 14:44:42 -0800

    first 3 women

--- Section_79_advanced_data_analysis/main.py
+++ Section_79_advanced_data_analysis/main.py
@@ -22,10 +22,12 @@ print(df.info())
 
 sex_data = df['sex'].value_counts()
 
-fig = plotly.pie(labels=sex_data.index,
-                 values=sex_data.values,
-                 title="Percentage M vs F",
-                 names=sex_data.index,
-                 hole=0.4)
-fig.update_traces(textposition='inside', textfont_size=15, textinfo='percent')
-fig.show()
+# fig = plotly.pie(labels=sex_data.index,
+#                  values=sex_data.values,
+#                  title="Percentage M vs F",
+#                  names=sex_data.index,
+#                  hole=0.4)
+# fig.update_traces(textposition='inside', textfont_size=15, textinfo='percent')
+# fig.show()
+
+print(df[df['sex'] == 'Female'].sort_values('year', ascending=True)[:3])
